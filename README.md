5e188b95cdbc59fd715b8ca6457fbda6d29c2c96
Guillaume Pinot <texitoi@texitoi.eu>
2014-09-29 00:44:30 +0200

    Licensing + doc + warning is error

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # 1||ΣwT [![Build Status](https://travis-ci.org/TeXitoi/1wT.svg?branch=master)](https://travis-ci.org/TeXitoi/1wT)
 
+## Description
+
 Some experiments on the scheduling problem 1||ΣwT.
+
+## License
+
+This work is free. You can redistribute it and/or modify it under the
+terms of the Do What The Fuck You Want To Public License, Version 2,
+as published by Sam Hocevar. See the COPYING file for more details.
--- src/prob.rs
+++ src/prob.rs
@@ -1,19 +1,32 @@
+// Copyright (c) 2014 Guillaume Pinot <texitoi(a)texitoi.eu>
+//
+// This work is free. You can redistribute it and/or modify it under
+// the terms of the Do What The Fuck You Want To Public License,
+// Version 2, as published by Sam Hocevar. See the COPYING file for
+// more details.
+
+//! Description of the problem
+
 use std::rand;
 use std::rand::distributions::{Sample, Range};
 
+/// A Job
 pub struct Job {
     /// processing time
     pub p: u32,
-    // due date
+    /// due date
     pub d: u32,
-    // weigth
+    /// weigth
     pub w: u32
 }
 
+/// The 1||ΣwT problem description
 pub struct Prob {
+    /// The list of the jobs
     pub jobs: Vec<Job>
 }
 
+/// Create a problem with `n` jobs.
 pub fn create_prob(n: u32) -> Prob {
     let rng: &mut rand::XorShiftRng =
         &mut rand::SeedableRng::from_seed([n, 2 * n, 3 * n, 5 * n]);
